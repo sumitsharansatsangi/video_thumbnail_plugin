e01b32b5d4513ffa4f701ab2b21fbaca9a98ff2e
sumitsharansatsangi <sharansumitkumar@gmail.com>
2025-02-04 09:33:23 +0530

    added parameter to the ios

--- ios/Classes/VideoThumbnailPlugin.swift
+++ ios/Classes/VideoThumbnailPlugin.swift
@@ -7,6 +7,9 @@ import MobileCoreServices
 import SDWebImageWebPCoder
 
 public class VideoThumbnailPlugin: NSObject, FlutterPlugin {
+    
+    private var channel: FlutterMethodChannel?
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "video_thumbnail_plugin", binaryMessenger: registrar.messenger())
         let instance = VideoThumbnailPlugin()
@@ -14,138 +17,146 @@ public class VideoThumbnailPlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      let webPCoder = SDImageWebPCoder.shared
-        SDImageCodersManager.shared.addCoder(webPCoder)
-        if call.method == "generateImageThumbnail" {
-            guard let args = call.arguments as? [String: Any],
-                  let videoPath = args["videoPath"] as? String,
-                  let thumbnailPath = args["thumbnailPath"] as? String else {
-                result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid arguments received", details: nil))
-                return
-            }
-            let width = args["width"] as? CGFloat
-            let height = args["height"] as? CGFloat
-            let format = args["format"] as? Int ?? 0
-            let quality = args["quality"] as? CGFloat ?? 100.0
-            
-            let thumbnail = generateImageThumbnail(videoPath: videoPath, thumbnailPath: thumbnailPath, width: width, height: height, format: format, quality: quality)
-            result(thumbnail)
-        } else if call.method == "generateGifThumbnail" {
-            guard let args = call.arguments as? [String: Any],
-                  let videoPath = args["videoPath"] as? String,
-                  let thumbnailPath = args["thumbnailPath"] as? String else {
-                result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid arguments received", details: nil))
-                return
-            }
-            let width = args["width"] as? CGFloat
-            let height = args["height"] as? CGFloat
-            let frameCount = args["frameCount"] as? Int ?? 10
-            let delay = args["delay"] as? Int ?? 100
-            let repeatCount = args["repeat"] as? Int ?? 0
-            
-            let gifPath = generateGifThumbnail(videoPath: videoPath, thumbnailPath: thumbnailPath, width: width, height: height, frameCount: frameCount, delay: delay, repeatCount: repeatCount)
-            result(gifPath)
-        } else {
+        switch call.method {
+        case "generateImageThumbnail":
+            handleImageThumbnail(call: call, result: result)
+        case "generateGifThumbnail":
+            handleGifThumbnail(call: call, result: result)
+        default:
             result(FlutterMethodNotImplemented)
         }
     }
     
-    private func generateImageThumbnail(videoPath: String, thumbnailPath: String, width: CGFloat?, height: CGFloat?, format: Int, quality: CGFloat) -> String? {
-        let asset = AVAsset(url: URL(fileURLWithPath: videoPath))
-        let generator = AVAssetImageGenerator(asset: asset)
-        generator.appliesPreferredTrackTransform = true
-        
-        do {
-            let cgImage = try generator.copyCGImage(at: CMTime(seconds: 1, preferredTimescale: 600), actualTime: nil)
-            var image = UIImage(cgImage: cgImage)
-            
-            if let width = width, let height = height {
-                image = image.resized(to: CGSize(width: width, height: height))
-            }
-            
-            let imageData: Data?
-            switch format {
-            case 0: imageData = image.pngData()
-            case 2: imageData = image.webpData(quality: CGFloat(quality) / 100.0) 
-            default: imageData = image.jpegData(compressionQuality: quality)
-            }
-            
-            if let data = imageData {
-                try data.write(to: URL(fileURLWithPath: thumbnailPath))
-                return thumbnailPath
-            }
-        } catch {
-            print("Error generating thumbnail: \(error)")
+    private func handleImageThumbnail(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let arguments = call.arguments as? [String: Any],
+              let videoPath = arguments["videoPath"] as? String,
+              let thumbnailPath = arguments["thumbnailPath"] as? String,
+              let width = arguments["width"] as? Int,
+              let height = arguments["height"] as? Int,
+              let format = arguments["format"] as? Int,
+              let quality = arguments["quality"] as? Int else {
+            result(FlutterError(code: "INVALID_ARGUMENT", message: "Missing required parameters", details: nil))
+            return
         }
-        return nil
+        
+        DispatchQueue.global(qos: .background).async {
+            let success = self.generateImageThumbnail(videoPath: videoPath, thumbnailPath: thumbnailPath, width: width, height: height, format: format, quality: quality)
+            DispatchQueue.main.async {
+                result(success)
+            }
+        }
     }
     
-    private func generateGifThumbnail(videoPath: String, thumbnailPath: String, width: CGFloat?, height: CGFloat?, frameCount: Int, delay: Int, repeatCount: Int) -> String? {
+    private func handleGifThumbnail(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let arguments = call.arguments as? [String: Any],
+              let videoPath = arguments["videoPath"] as? String,
+              let thumbnailPath = arguments["thumbnailPath"] as? String,
+              let width = arguments["width"] as? Int,
+              let height = arguments["height"] as? Int,
+              let frameCount = arguments["frameCount"] as? Int,
+              let delay = arguments["delay"] as? Int,
+              let repeatCount = arguments["repeat"] as? Int else {
+            result(FlutterError(code: "INVALID_ARGUMENT", message: "Missing required parameters", details: nil))
+            return
+        }
+        
+        DispatchQueue.global(qos: .background).async {
+            let success = self.generateGifThumbnail(videoPath: videoPath, thumbnailPath: thumbnailPath, width: width, height: height, frameCount: frameCount, delay: delay, repeatCount: repeatCount)
+            DispatchQueue.main.async {
+                result(success)
+            }
+        }
+    }
+    
+    private func generateImageThumbnail(videoPath: String, thumbnailPath: String, width: Int, height: Int, format: Int, quality: Int) -> Bool {
         let asset = AVAsset(url: URL(fileURLWithPath: videoPath))
-        let generator = AVAssetImageGenerator(asset: asset)
-        generator.appliesPreferredTrackTransform = true
+        let assetImageGenerator = AVAssetImageGenerator(asset: asset)
+        assetImageGenerator.appliesPreferredTrackTransform = true
         
-        let frameInterval = CMTime(seconds: asset.duration.seconds / Double(frameCount), preferredTimescale: 600)
-        let fileURL = URL(fileURLWithPath: thumbnailPath)
-
-        let gifType: CFString
-
-        if #available(iOS 14, *) {
-          gifType = UTType.gif.identifier as CFString
-        } else {
-          gifType = kUTTypeGIF
+        let time = CMTime(seconds: 1, preferredTimescale: 600)
+        
+        do {
+            let cgImage = try assetImageGenerator.copyCGImage(at: time, actualTime: nil)
+            var image = UIImage(cgImage: cgImage)
+            
+            image = resizeImage(image: image, width: width, height: height)
+            return saveImage(image: image, path: thumbnailPath, format: format, quality: quality)
+        } catch {
+            print("Error generating image thumbnail: \(error.localizedDescription)")
+            return false
         }
+    }
+    
+    private func generateGifThumbnail(videoPath: String, thumbnailPath: String, width: Int, height: Int, frameCount: Int, delay: Int, repeatCount: Int) -> Bool {
+        let asset = AVAsset(url: URL(fileURLWithPath: videoPath))
+        let assetImageGenerator = AVAssetImageGenerator(asset: asset)
+        assetImageGenerator.appliesPreferredTrackTransform = true
         
-        guard let destination = CGImageDestinationCreateWithURL(fileURL as CFURL, gifType, frameCount, nil) else {
-            return nil
-        }
+        let duration = asset.duration.seconds
+        let frameInterval = duration / Double(frameCount)
         
-        let properties = [
-            kCGImagePropertyGIFDictionary: [
-                kCGImagePropertyGIFLoopCount: repeatCount
+        let gifProperties: [String: Any] = [
+            kCGImagePropertyGIFDictionary as String: [
+                kCGImagePropertyGIFDelayTime as String: delay / 1000.0,
+                kCGImagePropertyGIFLoopCount as String: repeatCount
             ]
-        ] as CFDictionary
-        CGImageDestinationSetProperties(destination, properties)
+        ]
+        
+        let destinationURL = URL(fileURLWithPath: thumbnailPath)
+        let destination = CGImageDestinationCreateWithURL(destinationURL as CFURL, kUTTypeGIF, frameCount, nil)
         
         for i in 0..<frameCount {
-            let time = CMTimeMultiply(frameInterval, multiplier: Int32(i))
+            let time = CMTime(seconds: frameInterval * Double(i), preferredTimescale: 600)
             do {
-                let cgImage = try generator.copyCGImage(at: time, actualTime: nil)
+                let cgImage = try assetImageGenerator.copyCGImage(at: time, actualTime: nil)
                 var image = UIImage(cgImage: cgImage)
-                if let width = width, let height = height {
-                    image = image.resized(to: CGSize(width: width, height: height))
-                }
                 
-                let frameProperties = [
-                    kCGImagePropertyGIFDictionary: [
-                        kCGImagePropertyGIFDelayTime: Double(delay) / 1000.0
-                    ]
-                ] as CFDictionary
-                CGImageDestinationAddImage(destination, image.cgImage!, frameProperties)
+                image = resizeImage(image: image, width: width, height: height)
+                CGImageDestinationAddImage(destination!, image.cgImage!, gifProperties as CFDictionary)
             } catch {
-                print("Error extracting frame: \(error)")
+                print("Error generating GIF thumbnail: \(error.localizedDescription)")
+                return false
             }
         }
         
-        if CGImageDestinationFinalize(destination) {
-            return thumbnailPath
-        }
-        return nil
+        return CGImageDestinationFinalize(destination!)
     }
-}
-
-extension UIImage {
-    func resized(to size: CGSize) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        draw(in: CGRect(origin: .zero, size: size))
+    
+    private func resizeImage(image: UIImage, width: Int?, height: Int?) -> UIImage {
+        var newWidth = width
+        var newHeight = height
+        
+        if newWidth == nil && newHeight == nil {
+            return image
+        }
+        
+        if let newWidth = newWidth {
+            let aspectRatio = image.size.height / image.size.width
+            newHeight = Int(Double(newWidth) * aspectRatio)
+        }
+        
+        if let newHeight = newHeight {
+            let aspectRatio = image.size.width / image.size.height
+            newWidth = Int(Double(newHeight) * aspectRatio)
+        }
+        
+        UIGraphicsBeginImageContext(CGSize(width: newWidth!, height: newHeight!))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth!, height: newHeight!))
         let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return resizedImage ?? self
+        
+        return resizedImage ?? image
     }
-    func webpData(quality: CGFloat) -> Data? {
-        let options: [SDImageCoderOption: Any] = [
-            .compressionQuality: quality // Compression quality (0.0 - 1.0)
-        ]
-        return SDImageWebPCoder.shared.encodedData(with: self, format: .webP, options: options)
+    
+    private func saveImage(image: UIImage, path: String, format: Int, quality: Int) -> Bool {
+        guard let data = image.jpegData(compressionQuality: CGFloat(quality) / 100.0) else { return false }
+        
+        do {
+            try data.write(to: URL(fileURLWithPath: path))
+            return true
+        } catch {
+            print("Error saving image: \(error.localizedDescription)")
+            return false
+        }
     }
 }
