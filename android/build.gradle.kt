f983d554291a73af553bd13186e54505885ccdc3
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-09 18:55:08 +0530

    code updated

---
+++ android/build.gradle.kt
@@ -0,0 +1,70 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+group = "com.kumpali.video_thumbnail_plugin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+android {
+    namespace = "com.kumpali.video_thumbnail_plugin"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+
+        getByName("test") {
+            java.srcDirs("src/test/kotlin")
+        }
+    }
+
+    testOptions {
+        unitTests.all {
+            useJUnitPlatform()
+
+            testLogging {
+                events(
+                    "passed",
+                    "skipped",
+                    "failed",
+                    "standardOut",
+                    "standardError"
+                )
+
+                showStandardStreams = true
+            }
+
+            outputs.upToDateWhen {
+                false
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("com.github.bumptech.glide:gifencoder-integration:5.0.7")
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.mockito:mockito-core:5.23.0")
+}
